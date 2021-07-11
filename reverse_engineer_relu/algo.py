0febcd02d822e2c9bfcfcde3b0dd83df1419c88c
Pritindra <pritindra.d@gmail.com>
2021-07-11 21:15:34 +0530

    rusted layer 1

---
+++ reverse_engineer_relu/algo.py
@@ -0,0 +1,33 @@
+# first layer
+# network N, weight matrix W, bias vector b
+# hyperplane Bz for each z, W.x + b = 0
+p1 = []
+p2 = []
+s1 = []
+
+def PointsOnLine(l):
+    pass
+
+def InferHyperplane(p):
+    pass
+
+def TestHyperplane(H):
+    pass
+
+def GetParams(H):
+    pass
+
+def algo():
+    for t in range(1,L):
+        # sample line segment l
+        p1 = p1 + PointsOnline(l)
+    
+    for p in p1:
+        H = InferHyperplane(p)
+        if TestHyperplane(H):
+            s1 = s2 + GetParams(H)
+        else:
+            p2 = p2 + p
+    return p2, s1
+
+    
