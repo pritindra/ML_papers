cc38a802fb1dffc56ee5b3cc7cb4798875cab5f0
Pritindra <pritindra.d@gmail.com>
2021-07-14 19:57:04 +0530

    added dataset

---
+++ AlexNet/AlexNet.py
@@ -0,0 +1,15 @@
+"""
+Implementing AlexNet CNN architecture in a dataset of 5 letter
+captcha images .
+Dataset api :
+kaggle datasets download -d fournierp/captcha-version-2-images
+
+"""
+import numpy as np
+import cv2
+import pandas as pd
+import tensorflow as tf
+
+
+
+
