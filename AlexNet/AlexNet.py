a7eda5067ed81bafa2d781b7696e4fad84e7cf98
Pritindra <pritindra.d@gmail.com>
2021-07-15 21:09:11 +0530

    model improved

--- AlexNet/AlexNet.py
+++ AlexNet/AlexNet.py
@@ -12,7 +12,7 @@ import tensorflow as tf
 import os
 from PIL import Image
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-from sklearn.preprocessing import train_test_split
+from sklearn.model_selection import train_test_split
 path = "./samples"
 
 # Adaptive Thresh holding images
@@ -59,19 +59,17 @@ Y = np.array(Y)
 X /= 255.0
 
 y_comb = LabelEncoder().fit_transform(Y)
-y_one_hot = OneHotEncoder(sparse= False).fit_transform(y_combine.reshape(len(y_combine),1))
+y_one_hot = OneHotEncoder(sparse= False).fit_transform(y_comb.reshape(len(y_comb),1))
 
-X_train, X_test, y_train, y_test = train_test_split(X, y_one_hot, test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y_one_hot, test_size = 0.2, random_state=1)
 
 # convulational layer - 5
-def cn_layer(fx,y):
+def cn_layer(fx,y = False):
     model = tf.keras.models.Sequential()
     
     model.add(tf.keras.layers.Conv2D(fx,(3,3), activation="relu"))
-    if y = True:
+    if (y == True):
         model.add(tf.keras.layers.BatchNormalization())
-    else:
-        continue
     model.add(tf.keras.layers.Dropout(0.2))
     return model    
 
@@ -85,15 +83,15 @@ def fc_layer(hx,y):
 
 def cnn(f1,f2,f3,h1,h2):
     model = tf.keras.models.Sequential()
-    model.add(tf.keras.layers.Input(40,20,1,))
+    model.add(tf.keras.layers.Input((40,20,1,)))
 
     model.add(cn_layer(f1, True))
     model.add(tf.keras.layers.MaxPooling2D(pool_size = (2,2), padding = "same"))
     model.add(cn_layer(f2,True))
     model.add(tf.keras.layers.MaxPooling2D(pool_size = (2,2), padding = "same"))
-    model.add(cn_layer(f3,False))
-    model.add(cn_layer(f3,False))
-    model.add(cn_layer(f2,False))
+    model.add(cn_layer(f3))
+    model.add(cn_layer(f3))
+    model.add(cn_layer(f2))
     model.add(tf.keras.layers.MaxPooling2D(pool_size = (2,2), padding = "same"))
 
     model.add(tf.keras.layers.Flatten())
@@ -103,5 +101,16 @@ def cnn(f1,f2,f3,h1,h2):
     
     model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
     return model
+
+traingen = tf.keras.preprocessing.image.ImageDataGenerator(rotation_range=5, width_shift_range=[-2,2])
+traingen.fit(X_train)
+model = cnn(128, 32, 16, 32, 32)
+model.summary()
+
+reduce = ReduceLROnPlateau(monitor="val_loss", patience=20, verbose=1)
+checkp = ModelCheckpoint('./result_model.h5', monitor = 'val_loss', verbose = 1, save_best_only = True)
+
+history = model.fit(traingen.flow(X_train, y_train, batch_size = 32), validation_data = (X_test, y_test), epochs = 150, steps_per_epoch = len(X_train)/32, callbacks = [checkp])
+
 
 
