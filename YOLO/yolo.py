a07d0f780fcbdd0c8dc6068629f38fe6c6828613
Pritindra <pritindra.d@gmail.com>
2021-07-20 10:05:20 +0530

    initial

---
+++ YOLO/yolo.py
@@ -0,0 +1,14 @@
+"""
+You only look once : paper implementation
+
+"""
+
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import os
+
+train_dir = '/train_zip/train'
+test_dir = '/test_zip/test'
+
+
