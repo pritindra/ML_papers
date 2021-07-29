edaf28c456252dd3c28e30b8e5c69db49f9e5e41
Pritindra <pritindra.d@gmail.com>
2021-07-29 20:58:05 +0530

    doesnt work

--- YOLO/yolo.py
+++ YOLO/yolo.py
@@ -7,8 +7,12 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 import os
+import cv2
 
-train_dir = '/train_zip/train'
-test_dir = '/test_zip/test'
+train_dir = 'train_zip/train'
+test_dir = 'test_zip/test'
 
+images = [cv2.imread(image, cv2.IMREAD_UNCHANGED) for image in sorted(os.listdir(train_dir)) if image[-4:]=='.jpg']
+
+print(images[0].shape)
 
